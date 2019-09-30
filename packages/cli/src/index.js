5cb00dd86edce9ade89e131b9a02453e54a21b31
AmoDinho <u14284783@tuks.co.za>
2019-09-30 13:45:36 +0200

    okay added basic hellow world

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -3,11 +3,12 @@
 const program = require('commander')
 
 program
-    .command('naboo create')
     .option('-r, --react')
     .option('-v, --vue')
 
-
 program.parse(process.argv)
-if (program.react) console.log('install react lib')
-if (program.vue) console.log('install vue lib')
+
+if (program.react) program.action(console.log('install react lib'))
+if (program.vue) program.action(console.log('install vue lib'))
+
+
