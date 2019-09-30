7d87f18078b18afbbff1a9fa55a7470b7778673d
AmoDinho <u14284783@tuks.co.za>
2019-09-30 14:01:10 +0200

    export the cli

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const program = require('commander')
+exports.cli = () => {
 
-program
-    .option('-r, --react')
-    .option('-v, --vue')
+    program
+        .option('-r, --react')
+        .option('-v, --vue')
 
-program.parse(process.argv)
+    program.parse(process.argv)
 
-if (program.react) program.action(console.log('install react lib'))
-if (program.vue) program.action(console.log('install vue lib'))
+    if (program.react) program.action(console.log('install react lib'))
+    if (program.vue) program.action(console.log('install vue lib'))
 
 
+}
